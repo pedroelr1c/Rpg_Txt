65330f4ea403c136aef58d1eecd586507d8f79a1
pedro elric <joaopedroxbr14@gmail.com>
2023-07-02 15:39:18 -0300

    rpg de texto

---
+++ atb_player.c
@@ -0,0 +1,13 @@
+typedef struct{
+	
+
+	int i_pv=20;
+	int i_atk=5;
+	int i_def=2;
+	int i_nivel=1;
+	int i_xp=0;
+		
+
+		
+	}atb_player;
+
---
+++ slime_verde.c
@@ -0,0 +1,10 @@
+
+	typedef struct{
+		
+		char c_nome[50]="slime verde";
+		int i_pv_slm=20;
+		int i_atk_slm=1;
+		int i_def_slm=1;
+		
+	}slime_verde;
+
